79f53dcef8d12bfe87067068d95a2142b8ce2563
sajjad Alizadeh <samycrip10@gmail.com>
2021-07-27 16:15:01 +0430

    .

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module alaki
+module github.com/samblize-pk7/Golang-FirstProject.git
 
 go 1.16
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/samblize-pk7/Golang-FirstProject.git/alaki"
+
+func main() {
+	println("hello world")
+	alaki.Hi()
+}
